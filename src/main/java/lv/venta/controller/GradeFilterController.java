660be7bf6f35f17fccde10e8bf3ddbfb49e94b13
Ilja Avlass <epastsneeksiste@gmail.com>
2024-05-17 11:24:27 +0300

    Grades Controller filter done

--- src/main/java/lv/venta/controller/GradeFilterController.java
+++ src/main/java/lv/venta/controller/GradeFilterController.java
@@ -22,7 +22,7 @@ public class GradeFilterController {
 	@Autowired
 	private IGradesFilteringService gradeFilterService;
 	
-	@GetMapping("/failed") // localhost:8080/course/filter/failed
+	@GetMapping("/failed") // localhost:8080/grade/filter/failed
 	public String getGradesFiltredByFailedGrades(Model model) {
 		try {
 			ArrayList<Grade> selectGrades = gradeFilterService.selectFailedGrades();
@@ -35,10 +35,35 @@ public class GradeFilterController {
 		}
 	}
 	
+	@GetMapping("/student/{id}") // localhost:8080/grade/filter/student/{id}
+	public String getAllGradesByStudentID(@PathVariable("id") int id, Model model) {
+		try {
+			ArrayList<Grade> selectGrades = gradeFilterService.allGradesByStudentID(id);
+			model.addAttribute("mydata", selectGrades);
+			model.addAttribute("msg", "All student grades");
+			return "grades-show-all-page";
+		} catch (Exception e) {
+			model.addAttribute("", e.getMessage());
+			return "error-page";
+		}
+	}
 	
 	
-	
-	
+
+	@GetMapping("/avg/course/{id}") //localhost:8080/grade/filter/avg/course/{id}
+	public String getGradeFilterAVGByCourseId(@PathVariable("id") int id, Model model) {
+		try
+		{
+			float avgGrades = gradeFilterService.calculateAVGGradeInCourseID(id);
+			model.addAttribute("msg", "Avg grade: " + avgGrades);
+			return "grades-show-all-page";
+		}
+		catch (Exception e) {
+			model.addAttribute("mydata", e.getMessage());
+			return "error-page";
+
+		}
+	}
 	
 	
 }
--- src/main/java/lv/venta/repo/IGradeRepo.java
+++ src/main/java/lv/venta/repo/IGradeRepo.java
@@ -13,7 +13,7 @@ public interface IGradeRepo extends CrudRepository<Grade, Integer>{
 
 	ArrayList<Grade> findByStudentIds(int id);
 
-	@Query(nativeQuery = true, value = "select avg(grades) from grade_table where idc=?1;")
+	@Query(nativeQuery = true, value = "select avg(grades) from grade_table where idc=(?1);")
 	float calculateAVGGradeInCourseById(int id);
 
 }
